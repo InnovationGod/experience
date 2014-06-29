a4c64df95ac52be676097277574335013f9b7b61
InnovationGod <idar.douglas.hillgaar@live.com>
2014-06-29 18:06:08 +0100

    1st draft

---
+++ ExperienceData.py
@@ -0,0 +1,17 @@
+'''
+Created on 11 Mar 2013
+
+@author: eidahil
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
